d6dcfeb6cbb532a23b2ccafaafa65f15e73b1336
Meek Msaki <meek10x@gmail.com>
2025-12-08 19:14:18 -0600

    removes warning

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
 //! This crate provides a native LSP server for Solidity development using Foundry's
 //! compilation and linting infrastructure.
 
-#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 pub mod build;
